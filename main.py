a3d08897f0f1011b0c92d1e23743262d99fa56de
AnthonyTannous02 <anthony.tannous999@gmail.com>
2024-02-07 20:33:10 +0000

    merged

--- main.py
+++ main.py
@@ -57,4 +57,4 @@ def unauthorized_token_callback(error):
 from auth import bp as auth_bp
 app.register_blueprint(auth_bp, url_prefix="/auth")
 
-app.run(debug=True, host="0.0.0.0", port="5000", threaded=True, load_dotenv=True)
+# app.run(debug=True, host="0.0.0.0", port="5000", threaded=True, load_dotenv=True)
