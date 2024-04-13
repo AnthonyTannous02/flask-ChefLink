03cb94fa39ee043ff9bc359112e47399919f0975
AnthonyTannous02 <anthony.tannous999@gmail.com>
2024-04-13 19:49:30 +0300

    Tries New Deploy Method

--- main.py
+++ main.py
@@ -23,7 +23,9 @@ def run_app():
     app.config["SPRING_BOOT_URL"] = os.getenv("SPRING_BOOT_URL")
     app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
     app.config["JWT_COOKIE_SECURE"] = True
+    app.config["JWT_COOKIE_CSRF_PROTECT"] = False
     app.config["JWT_TOKEN_LOCATION"] = ["cookies"]
+    app.config["JWT_COOKIE_SAMESITE"] = "None"
     app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
     app.config["MONGO_URI"] = os.getenv("MONGO_URI")
 
